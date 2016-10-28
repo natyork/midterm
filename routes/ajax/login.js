8edb98488225d10d4d0e0b7c1e45b348836e200b
Patrick Simonian <patricksimonian@gmail.com>
2016-10-28 17:24:14 +0000

    added cookie sesion npm module, added comments to ajax/login.js

--- routes/ajax/login.js
+++ routes/ajax/login.js
@@ -1,22 +1,29 @@
 "use strict";
-//login route
+// ======login route======//
+
+
 //constants
 const express = require('express');
 const router  = express.Router();
 const bodyParser = require('body-parser');
 const db = require("../../lib/database/knex.js");
 //const response = require("../../lib/server/response.js");
-// const cookieSession = require('cookie-session');
+const cookieSession = require('cookie-session');
+
+
 //middlewares
 router.use(bodyParser.urlencoded({
   extended: false
 }));
-// router.use(cookieSession({
-//   name: 'session',
-//   keys: ['key1', 'key2']
-// }));
+router.use(cookieSession({
+  name: 'session',
+  //to be changed
+  secret: "liamneeson"
+}));
 
-//grab infromation submit and begins validation process
+
+
+//grab information from submit and begins validation process
 router.post("/login", (req, res) => {
   const email = req.body.email.trim();
   const pw = req.body.pw.trim();
@@ -24,13 +31,18 @@ router.post("/login", (req, res) => {
   const responseObj = new Object();
 
   if(email && pw) {
+
     db.findUserByCredentials(email, pw, (err, result) =>{
       if(err) {
+        //if error, send a status and a message
         responseObj["resStatus"] = 401;
         responseObj["msg"] = "error: login !";
-        console.log(err, "\n\n\nasdfasdf");
         res.json(responseObj);
       } else {
+        //set cookie by id
+        req.session.id = result.id;
+        //succesful login responds with status, msg as well
+        //as the relevant user information selected from the db
         responseObj["resStatus"] = 200;
         responseObj["msg"] = "Oh no!";
         responseObj["userid"] = result.id;
@@ -40,11 +52,10 @@ router.post("/login", (req, res) => {
       }
     });
   } else {
-    responseObj["resStatus"] = 403;
-    responseObj["msg"] = "Ohdd no!";
-    res.json(responseObj);
+      responseObj["resStatus"] = 403;
+      responseObj["msg"] = "Ohdd no!";
+      res.json(responseObj);
   }
-  // res.redirect("/cards");
 });
 
 //**let client handle redirects
