a74916626c9a2abebe4925bada0a1302f71d599b
Nat York <natyork@gmail.com>
2016-10-29 00:32:10 +0000

    cleaned up bootstrap layout for resources page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,28 +2,50 @@
 $(function() {
 
   function createResourceElement(resource) { //params are objects for info required
-    var $resource = $("<article>").addClass("resource item col-md-3");
-    var $header = $("<header>");
-    var $thumbnail = $("<img>").addClass("thumbnail").attr("src", resource.path);
-    var $iconHeart = $("<i>").attr({"class": "fa fa-heart", "aria-hidden": "true"});
-    var $visit = $("<a>").attr("href", resource.url).text("Link");
-    var $content = $("<section>").addClass("content");
-    var $title = $("<div>").addClass("title").text(resource.title);
-    var $description = $("<div>").addClass("description").text(resource.description);
-    var $user = $("<div>").addClass("user").text(resource.handle);
-    var $category = $("<div>").addClass("category").text(resource.name);
-    var $footer = $("<footer>");
+    var $resource = $("<article>").addClass("resource grid-item col-sm-6 col-md-4");
+    var $thumbnail = $("<div>").addClass("thumbnail");
+    var $img = $("<img>").addClass("thumbnail-img").attr("src", resource.path);
+    // var $iconHeart = $("<i>").attr({"class": "fa fa-heart", "aria-hidden": "true"});
+    // var $visit = $("<a>").attr("href", resource.url).text("Link");
+    // var $content = $("<section>").addClass("content");
+    var $caption = $("<div>").addClass("caption")
+    var $title = $("<h3>").text(resource.title);
+    var $description = $("<p>").addClass("description").text(resource.description);
+    var $iconHeart = $("<span>").attr({"class": "glyphicon glyphicon-heart", "aria-hidden": "true"});
+    // var $user = $("<div>").addClass("user").text(resource.handle);
+    // var $category = $("<div>").addClass("category").text(resource.name);
+    // var $footer = $("<footer>");
     // var $comments = $("<section>").addClass("comment-container"); //need to pull in comments
 
+    $caption = $caption.append($title).append($description).append($iconHeart);
+    $thumbnail = $thumbnail.append($img).append($caption);
+    $resource = $resource.append($thumbnail);
 
-    $header = $header.append($thumbnail).append($iconHeart).append($visit);
-    $content = $content.append($title).append($description).append($user).append($category)
-    // $footer = $footer.append($comments);
 
-    $resource = $resource.append($header).append($content).append($footer);
+
+    // $header = $header.append($thumbnail).append($iconHeart).append($visit);
+    // $content = $content.append($title).append($description).append($user).append($category)
+    // // $footer = $footer.append($comments);
+
+    // $resource = $resource.append($header).append($content).append($footer);
 
     return $resource;
   }
+
+// <div class="row"> y
+//   <div class="col-sm-6 col-md-4"> y
+//     <div class="thumbnail">
+//       <img src="..." alt="...">
+//       <div class="caption">
+//         <h3>Thumbnail label</h3>
+//         <p>...</p>
+//         <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+//       </div>
+//     </div>
+//   </div>
+// </div>
+
+
 
   function renderResources(arr) {
 
@@ -39,7 +61,7 @@ $(function() {
       var $resource = createResourceElement(resource);
       // $grid.append( $resource)
       // .masonry( 'appended', $resource );
-      $('.resource-container').prepend($resource);
+      $('.resource-row').prepend($resource);
     }
   }
 
