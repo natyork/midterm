c7bf29d96408ff675c4a8436aaf026ac4b626ccd
Patrick Simonian <patricksimonian@gmail.com>
2016-10-30 18:57:16 +0000

    began work on resource edit/delete functionality

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -21,8 +21,8 @@ function getUserInfo() {
     $.ajax({
       method: "get",
       url: "/api/resources/user/" + user.id
-    }).then(function (resources) {
-       renderUserResources(resources, ".resource-row", false);
+    }).then(function (responseObj) {
+       renderUserResources(responseObj.resources, ".resource-row", false, responseObj.admin);
     })
   });
 }
@@ -42,22 +42,23 @@ function getLikedResources() {
 
 //inserts a resource dom element into a parent with class of
 //parentClass
-function renderUserResources(arr, parentClass, toggleState) {
-
+function renderUserResources(arr, parentClass, toggleState, admin) {
+  var modalid = 0;
   for (i in arr) {
     var resource = arr[i];
-    var $resource = createResourceElement(resource, toggleState);
+    var $resource = createResourceElement(resource, toggleState, admin, modalid);
     setHeartColorStyle($resource);
     $(parentClass).prepend($resource);
+    modalid++;
   }
 }
-function createResourceElement(resource, toggleState) {
+function createResourceElement(resource, toggleState, admin, modalid) {
   var $resource = $(`<article class="col-sm-6 col-md-4"></article>`);
   $resource.attr({"data-resourceid" : resource.id});
   $resource.append(`
     <div class="thumbnail">
       <h3> ${resource.name}  <p class="handle"></p></h3>
-      <img  onclick="showMenu()" src="${resource.path}" >
+      <img src="${resource.path}" >
       <div class="caption">
         <h3>${resource.title}</h3>
         <p>${resource.description}</p>
@@ -70,14 +71,32 @@ function createResourceElement(resource, toggleState) {
   if(resource.handle) {
     $resource.find(".handle").html(resource.handle);
   }
+  if(admin === true) {
+    var imgElm = $resource.find("img");
+    $(`<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal${modalid}"">
+        edit
+      </button>   <div class="modal fade" id="myModal${modalid}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <h4 class="modal-title" id="myModalLabel">${resource.title}</h4>
+          </div>
+          <div class="modal-body thumbnail">
+            <img src="${resource.path}">
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>`).insertAfter(imgElm);
+  }
   return $resource;
 }
 
-// function setHeartColorStyle(resourceIn) {
-//   var heart = resourceIn.find(".glyphicon-heart");
-//   if(heart.data("istoggled") === true) {
-//     heart.css("color", "red");
-//   } else {
-//     heart.css("color", "black");
-//   }
-// }
+
+
